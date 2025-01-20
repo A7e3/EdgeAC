fe813829dbe088b182241fc09edb8f9deec44791
LWJGL <78892259+A7e3@users.noreply.github.com>
2025-01-20 11:26:18 -0800

    Create new-age.js

---
+++ new-age.js
@@ -0,0 +1,58 @@
+var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
+if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
+{
+  let window = _____WB$wombat$assign$function_____("window");
+  let self = _____WB$wombat$assign$function_____("self");
+  let document = _____WB$wombat$assign$function_____("document");
+  let location = _____WB$wombat$assign$function_____("location");
+  let top = _____WB$wombat$assign$function_____("top");
+  let parent = _____WB$wombat$assign$function_____("parent");
+  let frames = _____WB$wombat$assign$function_____("frames");
+  let opener = _____WB$wombat$assign$function_____("opener");
+
+(function() {
+  "use strict"; // Start of use strict
+
+  var mainNav = document.querySelector('#mainNav');
+
+  if (mainNav) {
+
+    var navbarCollapse = mainNav.querySelector('.navbar-collapse');
+    
+    if (navbarCollapse) {
+      
+      var collapse = new bootstrap.Collapse(navbarCollapse, {
+        toggle: false
+      });
+      
+      var navbarItems = navbarCollapse.querySelectorAll('a');
+      
+      // Closes responsive menu when a scroll trigger link is clicked
+      for (var item of navbarItems) {
+        item.addEventListener('click', function (event) {
+          collapse.hide();
+        });
+      }
+    }
+
+    // Collapse Navbar
+    var collapseNavbar = function() {
+
+      var scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+
+      if (scrollTop > 100) {
+        mainNav.classList.add("navbar-shrink");
+      } else {
+        mainNav.classList.remove("navbar-shrink");
+      }
+    };
+    // Collapse now if page is not at top
+    collapseNavbar();
+    // Collapse the navbar when page is scrolled
+    document.addEventListener("scroll", collapseNavbar);
+  }
+
+})(); // End of use strict
+
+
+}
